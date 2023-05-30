98787bf0b7b67cc72f81ebf30d6e2b3c6744864e
Marcus <marcusongkiansiong@gmail.com>
2023-05-30 13:07:42 +0800

    error: forgot to change from localhost database to remote database

--- tools/database/database.js
+++ tools/database/database.js
@@ -130,25 +130,25 @@ class client{
     }
 }   
 
-const obj = {
-    database: 'portfolio_website',
-    username: 'marcus',
-    password: "",
-    hostname: 'localhost',
-    port: '5432',
-  };
-  
 // const obj = {
-//     database: 'test_7mf4',
+//     database: 'portfolio_website',
 //     username: 'marcus',
-//     password: 'AyMxTaghIdB72sYVwXarfXrvXo3h3xXz',
-//     hostname: 'dpg-ch71mlg2qv26p1ajjkqg-a',
+//     password: "",
+//     hostname: 'localhost',
 //     port: '5432',
-//     connectionString: 'postgres://marcus:AyMxTaghIdB72sYVwXarfXrvXo3h3xXz@dpg-ch71mlg2qv26p1ajjkqg-a.singapore-postgres.render.com/test_7mf4',
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// }
+//   };
+  
+const obj = {
+    database: 'test_7mf4',
+    username: 'marcus',
+    password: 'AyMxTaghIdB72sYVwXarfXrvXo3h3xXz',
+    hostname: 'dpg-ch71mlg2qv26p1ajjkqg-a',
+    port: '5432',
+    connectionString: 'postgres://marcus:AyMxTaghIdB72sYVwXarfXrvXo3h3xXz@dpg-ch71mlg2qv26p1ajjkqg-a.singapore-postgres.render.com/test_7mf4',
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
 
 
 
